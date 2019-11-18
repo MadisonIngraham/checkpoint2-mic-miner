ae524551713cac10770c4ddf98570e80933e760b
MadisonIngraham <madi.ingraham@gmail.com>
2019-11-18 09:30:59 -0700

    automatic upgrades setup

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-var music = 0;
+let music = 0;
 function mine() {
   music += 1;
-  document.getElementById("music").innerHTML = music;
+  document.getElementById("music").innerHTML = ` <h1> ${music} </h1>`;
+}
+
+let clickUpgrades = {
+  fans: {
+    price: 75,
+    quantity: 0,
+    multiplier: 2
+  },
+  duet: {
+    price: 150,
+    quantity: 0,
+    multiplier: 5
+  }
+};
+
+let automaticUpgrades = {
+  silverMic: {
+    price: 300,
+    quantity: 0,
+    multiplier: 10
+  },
+  goldMic: {
+    price: 500,
+    quantity: 0,
+    multiplier: 20
+  }
+};
+
+function buyFans() {
+  if (music >= 75) {
+  }
 }
